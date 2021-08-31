3666c92ef1e2f109ba03e3ae0ade86658e4daa31
zhum <470876800@qq.com>
2021-08-31 17:41:31 +0800

    v2.0

---
+++ config/elaticsearch.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host' => env('ELASTICSEARCH_HOST', '127.0.0.1'),
+    'port' => env('ELASTICSEARCH_PORT', '9200'),
+];
+
+
---
+++ src/ElaticsearchServiceProvider.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Haode\Elaticsearch;
+
+use Haode\Elaticsearch\Console\ImportCommand;
+use Illuminate\Support\ServiceProvider;
+
+class ElaticsearchServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'haode');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'haode');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+            $this->commands([
+                ImportCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/elaticsearch.php', 'elaticsearch');
+
+        // Register the service the package provides.
+        $this->app->singleton('elaticsearch', function ($app) {
+            return new Elaticsearch;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['elaticsearch'];
+    }
+
+    /**
+     * Console-specific booting.
+     *
+     * @return void
+     */
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__.'/../config/elaticsearch.php' => config_path('elaticsearch.php'),
+        ], 'elaticsearch.config');
+
+        // Publishing the views.
+        /*$this->publishes([
+            __DIR__.'/../resources/views' => base_path('resources/views/vendor/haode'),
+        ], 'elaticsearch.views');*/
+
+        // Publishing assets.
+        /*$this->publishes([
+            __DIR__.'/../resources/assets' => public_path('vendor/haode'),
+        ], 'elaticsearch.views');*/
+
+        // Publishing the translation files.
+        /*$this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/haode'),
+        ], 'elaticsearch.views');*/
+
+        // Registering package commands.
+        // $this->commands([]);
+    }
+}
